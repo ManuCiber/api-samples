b0dd332defde9146f9a4d1d5adbe5f2685f218cd
= <86160611+ManuCiber@users.noreply.github.com>
2026-06-16 07:46:34 -0400

    Api config updated.

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,16 @@ import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { HttpExceptionFilter } from './common/filters/http_exception.filters';
 import { envs } from './config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+  app.enableCors();
+  
   const logger = new Logger('Sample - App');
   
   await app.listen(envs.PORT || 3000);
-  app.enableCors();
-  app.setGlobalPrefix('api');
   
   app.useGlobalPipes(
     new ValidationPipe({
@@ -21,6 +23,16 @@ async function bootstrap() {
   );
   
   app.useGlobalFilters(new HttpExceptionFilter());
+
+  const config = new DocumentBuilder()
+  .setTitle('Api Almacén Muestras Medicas')
+  .setDescription('Sistema Backend avanzado para la gestión de inventario y entrega de muestras medicas a visitadores medicos')
+  .setVersion('1.0.0')
+  .build()
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document)
+  
   logger.log(`Server running on port ${envs.PORT || 3000}`);
 }
 bootstrap();
